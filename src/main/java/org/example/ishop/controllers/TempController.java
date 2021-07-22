ce0a5e0bde586c0fe42ea6e7b5a303bfa9d1676c
derton1996 <zulusvlad18@gmail.com>
2021-07-22 16:10:06 +0700

    kinosearch

--- src/main/java/org/example/ishop/controllers/TempController.java
+++ src/main/java/org/example/ishop/controllers/TempController.java
@@ -1,13 +1,12 @@
 package org.example.ishop.controllers;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.example.ishop.dto.ConstructorDTO;
 import org.example.ishop.dto.ResultDTO;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.LinkedList;
@@ -17,14 +16,16 @@ import java.util.List;
 @RequestMapping("/temp")
 public class TempController {
 
+
     @GetMapping
     public String getTemp(Model model) {
-        model.addAttribute("resultDTO", new ResultDTO());
+        ArrayList<ConstructorDTO> list = new ArrayList<>();
+        model.addAttribute("resultDTO", list);
         return "temp";
     }
 
     @PostMapping("/create")
-    public String create(@ModelAttribute("resultDTO") ResultDTO resultDTO) {
+    public String create(@RequestBody String json) {
         return "temp";
     }
 }
